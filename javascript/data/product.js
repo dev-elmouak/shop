1c5f9a866d8c04f7706a115065ff227d754b3099
elmouak <developer14elmouak@gmail.com>
2023-12-14 16:41:18 +0100

    add data product to array

---
+++ javascript/data/product.js
@@ -0,0 +1,17 @@
+let product = [{
+  img: "/img/vecteezy_isolated-black-t-shirt-white-hanger_8847294.png",
+  name: "Black shirt",
+  price: "99Dh"
+},{
+  img: "/img/vecteezy_headphones-design-3d-rendering-for-product-mockup_17054078.png",
+  name: "headphones",
+  price: "150Dh"
+},{
+  img: "/img/pngegg (1).png",
+  name: "ball",
+  price: "150Dh"
+},{
+  img: "/img/pngegg.png",
+  name: "shirt",
+  price: "80Dh"
+}];
